1be76d5e98c680bcf18def3f1290ae5d9f49accd
Ivani <katarzyna.stosio@gmail.com>
2020-03-01 12:11:55 +0100

    added PageNavigation component and react router dom library

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+} from 'react-router-dom';
+
+import PageNavigation from '../PageNavigation/PageNavigation';
 
 function App(){
-    return <h1>Hello, world!</h1>
+    return (
+        <BrowserRouter>
+            <PageNavigation />
+            <h1>Hello, world!</h1>
+            <Switch>
+                <Route path="/read">
+                    <Home/>
+                </Route>    
+            </Switch>
+        </BrowserRouter>
+    )
 }
+
+function Home() {
+    return <h2>Home</h2>;
+}
+  
 
 export default App;
---
+++ client/src/components/PageNavigation/PageNavigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PageNavigation() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="przeczytane.html">Przeczytane</a></li>
+                <li>Nieprzeczytane</li>
+                <li><a href="dokupienia.html">Do kupienia</a></li>                
+            </ul>
+        </nav>
+    );
+}
+
+export default PageNavigation;
